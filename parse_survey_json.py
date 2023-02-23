7fdb0df691aeb9215d31edaa73f2b5cfa4416b3e
robertmetzker <89026322+robertmetzker@users.noreply.github.com>
2023-02-23 10:31:47 -0500

    Update parse_survey_json.py
    Added display of survey results for a test entry

--- parse_survey_json.py
+++ parse_survey_json.py
@@ -18,6 +18,7 @@ all_surveys = {"data": [
 , "per_page": 50, "page": 1, "total": 12, "links": {"self": "https://api.surveymonkey.com/v3/surveys?per_page=50&page=1"}
 }
 
+response_data = [{"id": "12376448666", "recipient_id": "6070975071", "collection_mode": "default", "response_status": "completed", "custom_value": "Jordan, S", "first_name": "1599387", "last_name": "TGW-SYSTEMS INC", "email_address": "", "ip_address": "107.4.69.163", "logic_path": {}, "metadata": {"contact": {"email": {"type": "string", "value": "laura.butrick@tgw-group.com"}, "first_name": {"type": "string", "value": "1599387"}, "last_name": {"type": "string", "value": "TGW-SYSTEMS INC"}, "custom_value": {"type": "string", "value": "Jordan, S"}, "custom_value2": {"type": "string", "value": "1/29/2021"}, "custom_value3": {"type": "string", "value": "438765"}, "custom_value4": {"type": "string", "value": "1/29/2021"}}}, "page_path": [], "collector_id": "399901743", "survey_id": "300747984", "custom_variables": {}, "edit_url": "https://www.surveymonkey.com/r/?sm=WbOxayZCcCXe7Fjdxfn81xe2XgJs2w8FmFrRNBzX0F_2BhQpe4F6U3Svjg2HgHwbn5", "analyze_url": "https://www.surveymonkey.com/analyze/browse/xDwfKEeYKlwB67d7Bs_2FYnO8R1V931r1XjG0Ezz12s0g_3D?respondent_id=12376448666", "total_time": 88, "date_modified": "2021-02-02T19:34:16+00:00", "date_created": "2021-02-02T19:32:47+00:00", "href": "https://api.surveymonkey.com/v3/surveys/300747984/responses/12376448666", "pages": [{"id": "154390018", "questions": [{"id": "605148537", "answers": [{"choice_id": "3982038472"}]}, {"id": "605148539", "answers": [{"choice_id": "3982038484"}]}, {"id": "605148542", "answers": [{"choice_id": "3982038498"}]}, {"id": "605148543", "answers": [{"choice_id": "3982038503"}]}, {"id": "605148544", "answers": [{"choice_id": "3982038508"}]}, {"id": "605148545", "answers": [{"choice_id": "3982038511"}]}, {"id": "605148546", "answers": [{"choice_id": "3982038514"}]}, {"id": "605148547", "answers": [{"choice_id": "3982038520"}]}, {"id": "605148548", "answers": [{"choice_id": "3982038523"}]}, {"id": "605148549", "answers": [{"choice_id": "3982038528"}]}, {"id": "605148550", "answers": [{"choice_id": "3982038534"}]}]}]},]
 
 # create a dataclass to hold the survey data
 @dataclass
@@ -58,6 +59,9 @@ def show_questions(detail_info):
     for question in detail_info['pages'][0]['questions']:
         print( f'QUESTION ({question["position"]}): {question["id"]} : {question["headings"][0]["heading"]}' )
 
+        if "other" in question.get("answers","None"):
+            print( f'\t ANSWER: {question["answers"]["other"]["id"]} : {question["answers"]["other"]["text"]}' )
+
         try:
             for answer in question["answers"]["choices"]:
                 print( f'\t ANSWER: {answer["id"]} : {answer["text"]}' )
@@ -77,18 +81,16 @@ def add_question_answers_to_df(df: dict, detail_info: dict):
 
         if question["subtype"] != "essay":
 
-            if "other" in question["answers"]:
-                try:
-                    ans_id = question["answers"]["other"]["id"]
-                    ans[ans_id] = question["answers"]["other"]["text"]
-                except:
-                    pass
+            if "other" in question.get("answers","None"):
+                ans_id = question["answers"]["other"]["id"]
+                ans[ans_id] = question["answers"]["other"]["text"]
 
-            try:
-                for answer in question["answers"]["choices"]:
-                    ans[answer["id"]] = answer["text"]
-            except:
-                    ans[answer["id"]] = 'NO ANSWERS AVAILABLE'
+            if "choices" in question.get("answers","None"):
+                try:
+                    for answer in question["answers"]["choices"]:
+                        ans[answer["id"]] = answer["text"]
+                except:
+                        ans[answer["id"]] = 'NO ANSWERS AVAILABLE'
         else:
             ans[answer["id"]] = 'Free Form Field'
 
@@ -98,6 +100,23 @@ def add_question_answers_to_df(df: dict, detail_info: dict):
 
         # print(survey_info )
     return survey_info 
+
+
+def show_responses(response_data, parsed_questions):
+    # print( response_data )
+    print('Recipent: ', response_data[0]["recipient_id"]) 
+    print('FName: ', response_data[0]["first_name"]) 
+    print('LName: ', response_data[0]["last_name"]) 
+    print('Email: ', response_data[0]["metadata"]["contact"]["email"]["value"]) 
+
+    print('SurveyID: ', response_data[0]["survey_id"]) 
+    # print('Answers: ', response_data[0]["pages"][0]["questions"]) 
+
+    for answer in response_data[0]["pages"][0]["questions"]:
+        qid = answer["id"]
+        aid = answer["answers"][0]["choice_id"]
+        print( f'QUESTION: {qid},  {parsed_questions[qid]}' )
+        print( f'  ANSWER: {aid}, {parsed_questions[aid]}' )
 
 
 def main():
@@ -128,6 +147,9 @@ def main():
     show_questions( detail_info )
     parsed_questions = add_question_answers_to_df(survey_info, detail_info)
     print( parsed_questions )
+
+    print(f'\n\n##- Parsing example response data --')
+    show_responses( response_data, parsed_questions )
 
 if __name__ == "__main__":
     main()
