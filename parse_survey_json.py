2d90505fba142d230221fb3efcb6c9ef7bb7a1b4
robertmetzker <89026322+robertmetzker@users.noreply.github.com>
2023-02-27 16:08:22 -0500

    Update parse_survey_json.py
    added SQL outputters

--- parse_survey_json.py
+++ parse_survey_json.py
@@ -33,7 +33,7 @@ class Survey:
     def __post_init__(self):
         self.detail_url = self.href + '/details'                    # Details url containing the questions and answera available for the survey
         self.responses_url = self.href + '/responses/bulk'          # Responses url containing the responses for the survey
-
+        self.title = self.title.replace("'","''")
 
 
 
@@ -86,6 +86,10 @@ def add_question_answers_to_dict(df: dict, detail_info: dict):
                         ans[answer["id"]] = answer["text"]
                 except:
                         ans[answer["id"]] = 'NO ANSWERS AVAILABLE'
+
+            if "tag_data" in  question.get("answers","None"):
+                ans[answer["id"]] = answer["answers"][0]["text"] 
+
         else:
             ans[answer["id"]] = 'Free Form Field'
 
@@ -124,21 +128,115 @@ def show_responses(response_data, parsed_questions):
 def add_response_to_dict(response_info: dict, response_data: dict):
     # { 'recipent':'6070975071' ,'fname':'1599387', 'lname':'TGW-SYSTEMS INC', 'email':'laura.butrick@tgw-group.com', 'surveyid':'300747984', 'responses': {'605148537':'3982038472', '605148539':'3982038484', '605148542':'3982038498', '605148543':'3982038503', '605148544':'3982038508', '605148545':'3982038511', '605148546':'3982038514', '605148547':'3982038520', '605148548':'3982038523', '605148549':'3982038528', '605148550':'3982038534'} }
     df = response_info.copy()
-    df["recipent"] = response_data["recipient_id"]
+
+    df["person"] = response_data["recipient_id"]
     df["fname"] = response_data["first_name"]
     df["lname"] = response_data["last_name"]
     df["email"] = response_data["metadata"]["contact"]["email"]["value"]
     df["surveyid"] = response_data["survey_id"]
+    df["surveydate"] = response_data["date_created"]
+    df["totaltime"] = response_data["total_time"]
 
     response_dict = {}
     for answer in response_data["pages"][0]["questions"]:
         qid = answer["id"]
         aid = answer["answers"][0].get("choice_id","None")
-        response_dict[qid] = aid
+        if "tag_data" in answer["answers"][0]:
+            response_dict[qid] = answer["answers"][0]["text"]            
+        else:
+            response_dict[qid] = aid
     
     df["responses"] = response_dict
     # print(df)
     return df
+
+
+def output_questions( detail_info ):
+    print(f'\n\n--- INSERT STATEMENTS for QUESTIONS ---')
+    surveyid = detail_info['id']
+
+    for question in detail_info['pages'][0]['questions']:
+        qid = question["id"]
+        qno = question["position"]
+        qtxt =  question["headings"][0]["heading"].replace("'","''")
+ 
+        print( f"insert into SURVEY_MONKEY.QUESTIONS values( {surveyid!r}, {qno},  {qid!r},'{qtxt}' );" )
+
+
+def output_answers( detail_info ):
+    
+    print('-'*20)
+    print( '\n\n--- PRINTING ANSWERS ---')
+    surveyid = detail_info['id']
+
+
+    for question in detail_info['pages'][0]['questions']:
+        qid = question["id"]
+        if "other" in question.get("answers","None"):
+            aid = question["answers"]["other"]["id"]
+            atxt = question["answers"]["other"]["text"]
+
+
+        try:
+            for answer in question["answers"]["choices"]:
+                aid = answer["id"]
+                atxt = answer["text"]
+                atxt = atxt.replace("'","''").replace("\n"," ").replace("\r"," ")
+                print( f"insert into SURVEY_MONKEY.ANSWERS values(  {surveyid!r}, {qid!r},  {aid!r},{atxt!r} );" )
+        except:
+            print( f"insert into SURVEY_MONKEY.ANSWERS values(  {surveyid!r}, {qid!r},  {aid!r},{atxt!r} );" )
+
+        if "other" in question.get("answers","None"):
+            aid = question["answers"]["other"]["id"]
+            atxt = question["answers"]["other"]["text"]
+            atxt = atxt.replace("\n"," ").replace("\r"," ")
+            atxt = atxt.replace("'","''")
+
+            print( f"insert into SURVEY_MONKEY.ANSWERS values(  {surveyid!r}, {qid!r},  {aid!r},{atxt!r} );" )
+
+
+def output_participants(all_responses):
+    '''
+    [{'recipent': '6070975071', 'fname': '1599387', 'lname': 'TGW-SYSTEMS INC', 'email': 'laura.butrick@tgw-group.com', 'surveyid': '300747984', 'surveydate': '2021-02-02T19:32:47+00:00', 'totaltime': 88, 'responses': {'605148537': '3982038472', '605148539': '3982038484', '605148542': '3982038498', '605148543': '3982038503', '605148544': '3982038508', '605148545': '3982038511', '605148546': '3982038514', '605148547': '3982038520', '605148548': '3982038523', '605148549': '3982038528', '605148550': '3982038534'}},
+    PARTICIPANT_ID  text,
+    FIRST_NAME      text,
+    LAST_NAME       text,
+    EMAIL           text
+    '''
+    stmt = 'insert into SURVEY_MONKEY.PARTICIPANTS values ('
+
+    print("--- INSERT STATEMENTS for PARTICIPANTS ---")
+    for person in all_responses:
+        person_dict = dict( person )
+        print( f"{stmt} {person_dict.get('person')!r}, {person_dict.get('fname')!r}, {person_dict.get('lname')!r}, {person_dict.get('email')!r} );" )
+
+
+def output_responses(all_responses):
+    '''
+    SURVEY_ID       text,    --300747984
+    PARTICIPANT_ID  text,
+    QUESTION_ID     text,
+    ANSWER_ID       text,
+    ANSWER_TXT      text,   -- For Free Form
+    '''
+    stmt = 'insert into SURVEY_MONKEY.RESPONSES values ('
+
+    print("\n\n--- INSERT STATEMENTS for RESPONSES ---")
+    for person in all_responses:
+        # print(person)
+        # print('---')
+
+        try:
+            person_dict = dict( person )
+            ans_list = person_dict.get('responses',"")
+            for q,a  in  ans_list.items() :
+                ans = a
+                if not a: ans= 'None'
+                ans.replace("'","''").replace("\n"," ")
+                print( f"{stmt} {person_dict.get('surveyid')!r}, {person_dict.get('person')!r}, {q!r}, '{ans}' );" )
+        except:
+            print( f'##### ERROR:  {person}')
+
 
 def main():
     # create a list of Survey objects
@@ -147,7 +245,8 @@ def main():
     #print survey titles
     print(f'--SURVEYS available: {len(surveys)} --')
     for survey in surveys:
-        print(survey.id, ':', survey.title)
+        print( f"insert into SURVEY_MONKEY.SURVEYS values ( {survey.id!r}, '{survey.title}' );" )
+        # print(survey.id, ':', survey.title)
     print('-'*40)
 
     detail1 = 'OHIO_BUREAU_OF_WORKERS_COMPENSATION_EMPLOYER_AUDIT_SURVEY_details.json'
@@ -169,27 +268,35 @@ def main():
     print( survey_info )
     print('-'*40)
 
-    show_questions( detail_info )
+    # show_questions( detail_info )
+    output_questions( detail_info )
     parsed_questions = add_question_answers_to_dict(survey_info, detail_info)
     # print( parsed_questions )
 
+    output_answers( detail_info )
+
     print(f'\n\n##- Parsing example response data --')
     all_response_data = response_info[0]['data']
+
+    # Test a single response
     response_data = all_response_data[1]
     # print(response_data)
-    show_responses( response_data, parsed_questions )
+    # show_responses( response_data, parsed_questions )
 
     response_dict = {}
     all_responses = []
-    # Test a single response
-
-
     print('\n## Converting response to dictionary')
-    # response_dict = add_response_to_dict(response_dict, response_data)
-    # for response in all_response_data:
-    #     response_dict = add_response_to_dict(response_dict, response)
-    #     all_responses.append(response_dict)
+    response_dict = add_response_to_dict(response_dict, response_data)
+
+    for response in  all_response_data:
+        response_dict = add_response_to_dict(response_dict, response)
+        all_responses.append(response_dict)
+
     # print(all_responses)
+    output_participants(all_responses)
+    output_responses(all_responses)
+    
+
 
 if __name__ == "__main__":
     main()
